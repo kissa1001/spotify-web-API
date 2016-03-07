dd03ff8ce1e69faa63cb1e807c1e544d418d77f8
kissa1001 <kissa101295@gmail.com>
2016-03-07 18:24:35 -0500

    parallelizing IO

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var getFromApi = function(endpoint, args) {
     unirest.get('https://api.spotify.com/v1/' + endpoint)
            .qs(args)
            .end(function(response) {
-
                 if (response.ok) {
                     emitter.emit('end', response.body);
                 }
@@ -29,13 +28,30 @@ app.get('/search/:name', function(req, res) {
 
     searchReq.on('end', function(item) {
         var artist = item.artists.items[0];
-        //res.json(artist);
         var relatedEndpoint = 'artists/'+artist.id+'/related-artists';
         var relatedReq = getFromApi(relatedEndpoint, {});
         
         relatedReq.on('end', function(item){
             artist.related = item.artists;
-            res.json(artist);
+
+            var artistsCompleted =0;
+            var checkComplete = function() {
+                if (artistsCompleted === artist.related.length) {
+                   res.json(artist);                   
+                }
+            }           
+        
+            artist.related.forEach(function (relatedArtist, index) {            
+               var topTracksEp = 'artists/' + relatedArtist.id + '/top-tracks';
+               var topTracksReq = getFromApi(topTracksEp, {'country':'US'});
+               
+                topTracksReq.on('end', function(item) {
+                   artist.related[index].tracks = item.tracks;
+                   artistsCompleted += 1;
+                   checkComplete();
+                });
+            });
+
         });
         
         relatedReq.on('error', function(code){
